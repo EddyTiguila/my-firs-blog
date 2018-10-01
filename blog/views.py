ad49fb8fdacfb6f5410408468ffca270a5d75c5e
Eddy Tiguila <eddyjose-th@hotmail.es>
2018-10-01 11:20:02 -0600

    se agrego

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+
 from .models import Publicacion
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def listado (request):
     publicados = Publicacion.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
     return render (request, 'blog/listar.html',{'publicados':publicados})
+def detalle (request, pk):
+     pub = get_object_or_404(Publicacion, pk=pk)
+     return render(request, 'blog/detalle.html', {'pub': pub})
