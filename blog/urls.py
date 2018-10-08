418313839cae8bd87eecd124cc55e7c230cc99ee
Eddy Tiguila <eddyjose-th@hotmail.es>
2018-10-08 12:17:13 -0600

    se agrego nuevo Post y Editar

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from .  import views
 
 urlpatterns =[
-url(r'^$', views.listado),
+url(r'^$', views.listado, name="listado"),
 url(r'^post/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
+url(r'^post/nuevo/$', views.nuevoPost, name='nuevoPost'),
+url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 
 from .models import Publicacion
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from .forms import NuevoForm
+from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 
 def listado (request):
@@ -10,3 +11,27 @@ def listado (request):
 def detalle (request, pk):
      pub = get_object_or_404(Publicacion, pk=pk)
      return render(request, 'blog/detalle.html', {'pub': pub})
+def nuevoPost(request):
+        if request.method == "POST":
+            form = NuevoForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicacion = timezone.now()
+                post.save()
+                return redirect('listado')
+        else:
+            form = NuevoForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+        post = get_object_or_404(Publicacion, pk=pk)
+        if request.method == "POST":
+            form = NuevoForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('listado')
+        else:
+            form = NuevoForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
